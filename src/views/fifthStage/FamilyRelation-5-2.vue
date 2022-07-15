d46a3980af5f343bdbd445ce6e5edcfd4d95161d
shiei35049 <109322814+shiei35049@users.noreply.github.com>
2022-07-15 16:42:56 +0800

    Update FamilyRelation-5-2.vue
    放入諮詢區塊程式碼

--- src/views/fifthStage/FamilyRelation-5-2.vue
+++ src/views/fifthStage/FamilyRelation-5-2.vue
@@ -56,19 +56,22 @@
             </div>
           </div>
         </section>
+        <Consult></Consult>
         <Circle></Circle>
       </main>
     </div>
 </template>
 <script>
-
+import Consult from '../../components/Consult.vue'
 export default {
   data () {
     return {
-      data: [
-
-      ]
     }
+  },
+  mounted () {
+  },
+  components: {
+    Consult
   }
 }
 </script>
