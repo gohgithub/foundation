5d96535987a320e5610ee78de1f0d7b0a46728f4
shiei35049 <109322814+shiei35049@users.noreply.github.com>
2022-07-15 16:31:33 +0800

    Update Study-5-1.vue
    放入諮詢區塊程式碼

--- src/views/fifthStage/Study-5-1.vue
+++ src/views/fifthStage/Study-5-1.vue
@@ -67,20 +67,22 @@
             </div>
           </div>
         </section>
+        <Consult></Consult>
+        <Circle></Circle>
       </main>
     </div>
 </template>
-
 <script>
+import Consult from '../../components/Consult.vue'
 export default {
   data () {
     return {
     }
   },
   mounted () {
-
   },
   components: {
+    Consult
   }
 }
 </script>
