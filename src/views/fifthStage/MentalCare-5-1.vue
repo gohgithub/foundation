487ddd063bd52b786ae6bf0eba2ce0111db90e53
nininiyou <nininiyou@gmail.com>
2022-07-29 16:38:15 +0800

    MentalCare-5-1

--- src/views/fifthStage/MentalCare-5-1.vue
+++ src/views/fifthStage/MentalCare-5-1.vue
@@ -152,19 +152,22 @@
             </div>
           </div>
         </section>
+        <Consult></Consult>
         <Circle></Circle>
       </main>
     </div>
 </template>
-
 <script>
+import Consult from '../../components/Consult.vue'
 export default {
   data () {
     return {
-      data: [
-
-      ]
     }
+  },
+  mounted () {
+  },
+  components: {
+    Consult
   }
 }
 </script>
