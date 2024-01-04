f5920f5a3b73ac1aeda18938b25e2f30b5cf5512
Eric <eric.yen@berryan.com>
2024-01-04 11:54:35 +0800

    deploy

---
+++ js/chunk-2d224901.bd0a9e45.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d224901"],{e154:function(n,e,t){"use strict";t.r(e);var c=t("7a23");function o(n,e,t,o,r,u){return Object(c["openBlock"])(),Object(c["createElementBlock"])("div",null," TEST ")}var r={data:function(){return{}},created:function(){console.log(this.$route.query)}},u=t("d959"),a=t.n(u);const d=a()(r,[["render",o]]);e["default"]=d}}]);
+//# sourceMappingURL=chunk-2d224901.bd0a9e45.js.map
